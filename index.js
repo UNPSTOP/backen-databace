a156c78a5849bfcb1f12c8ede305ef9485b6f384
Irfan aslam <irfanaslamo700@gmail.com>
2025-08-13 23:26:19 +0530

    Update

--- index.js
+++ index.js
@@ -8,20 +8,8 @@ const connectDB = require("./config/DB");
 const product = require("./routs/productrouter");
 const port = 4000;
 connectDB();
-app.use(cors({
-    origin: [
-        "https://my-gym-beryl.vercel.app",
-        "https://my-gym-beryl.vercel.app/register",
-        "https://my-gym-beryl.vercel.app/login",
-        "https://my-gym-beryl.vercel.app/Newpassord",
-        "http://localhost:5173/"
-        // "https://my-gym-beryl.vercel.app/Newpassord"
 
-    ],
-
-    methods: ["GET", "POST", "PUT"],
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/product', product);
